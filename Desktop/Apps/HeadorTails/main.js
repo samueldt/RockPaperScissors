f8842e7833bd816721af1d34ceb24b56b31ffcf4
Samuel Trahan <samtrahan91@yahoo.com>
2019-06-24 15:30:25 -0500

    heads or Tails

---
+++ Desktop/Apps/HeadorTails/main.js
@@ -0,0 +1,21 @@
+function findResult(myChoice) {
+  var userChoice = getYourChoice();
+  getYourResult(myChoice, userChoice);
+  document.querySelector(".result").innerHTML = getYourResult(
+    myChoice,
+    userChoice
+  );
+}
+
+function getYourChoice() {
+  var choices = ["Heads", "Tails"];
+  var randomChoice = Math.floor(Math.random() * 2);
+  return choices[randomChoice];
+}
+
+function getYourResult(myChoice, userChoice) {
+  if (userChoice == "Heads") {
+    return getYourChoice();
+  }
+  console.log(getYourResult);
+}
