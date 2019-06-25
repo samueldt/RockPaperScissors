56340f6bd08cb262a60f0c09766f245616713578
Samuel Trahan <samtrahan91@yahoo.com>
2019-06-25 15:21:13 -0500

    HeadsorTails

--- Desktop/Apps/HeadorTails/main.js
+++ Desktop/Apps/HeadorTails/main.js
@@ -5,6 +5,13 @@ function findResult(myChoice) {
     myChoice,
     userChoice
   );
+  var flips = parseInt(document.querySelector(".flip").innerHTML);
+  var success = parseInt(document.querySelector(".wins").innerHTML);
+  document.querySelector(".flip").innerHTML = flips + 1;
+  if (userChoice === myChoice) {
+    document.querySelector(".wins").innerHTML = success + 1;
+  }
+  console.log(myChoice, userChoice);
 }
 
 function getYourChoice() {
@@ -14,7 +21,10 @@ function getYourChoice() {
 }
 
 function getYourResult(myChoice, userChoice) {
-  if (userChoice == "Heads") {
+  if (userChoice === "Heads") {
+    return getYourChoice();
+  }
+  if (userChoice === "Tails") {
     return getYourChoice();
   }
 }
